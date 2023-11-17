814f9f2f6925cbaf0a2683a6368fe82520dc75cb
Ivan Fomin <ivfmn1@gmail.com>
2023-11-18 00:11:35 +0300

    hooks

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import _ from 'lodash';
+import './style.css';
+
+
+const req = new XMLHttpRequest();
+req.open('GET', 'http://ip.jsontest.com/');
+req.onload = function() {
+  const result = JSON.parse(req.response);
+  console.log('Done. Got', result);
+};
+req.send();
+
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+
+function getJSONdata(url, callback) {
+  console.log('getting JSON');
+  const req = new XMLHttpRequest();
+
+  req.open('GET', url);
+
+  req.onload = function() {
+    const result = JSON.parse(req.response);
+    console.log('Done, result:', result);
+    if (callback) {
+      callback(result);
+    }
+  };
+
+  req.onerror = function() {
+    console.log('Error! Status: ', req.status);
+  };
+
+  req.send();
+  console.log('Status: ', req.status);
+};
+
+function displayData(data) {
+  console.log(data);
+  let items = '';
+  data.forEach((item)=> {
+    const item_ = `<tr><td class="post-list">${item.title} by ${item.author}</td></tr>`;
+    console.log(item_);
+    items = items + item_;
+  });
+  console.log(items);
+
+  const displayNode = document.createElement('table');
+  const tHead = document.createElement('tHead');
+  tHead.innerHTML = 'JSON Data:\n';
+  displayNode.innerHTML = items;
+  displayNode.appendChild(tHead);
+
+  document.body.appendChild(displayNode);
+}
+
+
+document.body.appendChild(component());
+getJSONdata('http://localhost:3001/posts/', displayData);
+
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {static:'./dist'},
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: [
+                {
+                  loader: MiniCssExtractPlugin.loader,
+                  options: {
+                    esModule: true,
+                  },
+                },
+                'css-loader',
+           ],
+          },
+        ],
+      },
+    
+}
